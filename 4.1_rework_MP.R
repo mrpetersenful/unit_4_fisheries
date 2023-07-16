dc59b21a4e5bedd1e6fdb59e523e71bb753c82e9
mrpetersenful <mpetersen@seoe.sc.edu>
2023-07-15 21:24:52 -0400

    Create 4.1_rework_MP.R
    Knitted Unit 4 exercises, started 4.1

---
+++ 4.1_rework_MP.R
@@ -0,0 +1,5 @@
+### Lesson 4.1: Joins and shape
+## New functions: left_join(), right_join(), inner_join(), full_join(), semi_join(), anti_join(), gather(), spread()
+
+## Fisheries relational database
+
